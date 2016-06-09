2a3e9a57532cd72a446492e495ffb6f34261e123
platenspeler <mw12554@hotmail.com>
2016-06-09 18:52:45 +0200

    Release 1.2; Updates the license type of the Ideetron files used. Also made some changes to the lmic.cpp file and aes.cpp to better handle the TTN1.0 environment.,

--- libraries/lmic-v1.51/src/lmic/Encrypt_V30.h
+++ libraries/lmic-v1.51/src/lmic/Encrypt_V30.h
@@ -1,12 +1,27 @@
 /******************************************************************************************
+* Copyright 2015, 2016 Ideetron B.V.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************************/
+
+/******************************************************************************************
 *
 * File:        Encrypt_V30.h
 * Author:      Gerben den Hartog
 * Compagny:    Ideetron B.V.
 * Website:     http://www.ideetron.nl/LoRa
 * E-mail:      info@ideetron.nl
-* 
-* This file is distributed under GPL 3 license
 *
 ******************************************************************************************/
 /****************************************************************************************
@@ -35,7 +50,7 @@
 
 void Calculate_MIC(unsigned char *Data, unsigned char *Final_MIC, unsigned char Data_Length, unsigned int Frame_Counter, unsigned char Direction);
 void Encrypt_Payload(unsigned char *Data, unsigned char Data_Length, unsigned int Frame_Counter, unsigned char Direction);
-void Generate_Keys(unsigned char *K1, unsigned char *K2);
+void Generate_Keys(unsigned char *K1, unsigned char *K2, bool joining);
 void Shift_Left(unsigned char *Data);
 void XOR(unsigned char *New_Data,unsigned char *Old_Data);
 
--- libraries/lmic-v1.51/src/lmic/config.h
+++ libraries/lmic-v1.51/src/lmic/config.h
@@ -1,6 +1,7 @@
 #ifndef _lmic_config_h_
 #define _lmic_config_h_
 
+#define LOWPOWER 1
 // In the original LMIC code, these config values were defined on the
 // gcc commandline. Since Arduino does not allow easily modifying the
 // compiler commandline, use this file instead. (MK)
@@ -10,8 +11,17 @@
 //#define CFG_sx1272_radio 1
 #define CFG_sx1276_radio 1
 
-// 50 μs per tick
-#define US_PER_OSTICK 50
+#if defined(__AVR__)
+#define US_PER_OSTICK 30		// 50 works for Atmega 328/16MHz with little debug messaging. 30 seems to be working better
+
+#elif defined(ARDUINO_ARCH_ESP8266)
+#define US_PER_OSTICK 20		// To be determined
+
+#elif defined(__MKL26Z64__) || defined(__MK20DX128__)
+#define US_PER_OSTICK 20		// 35 works for mini-AES, 25 Works with original AES mode
+
+#endif
+
 #define OSTICKS_PER_SEC (1000000 / US_PER_OSTICK)
 
 #endif // _lmic_config_h_
